0abc8c88e5c6ed511e3a376f5bf46b5207b3f13a
philtd5000 <philiptaft9@gmail.com>
2018-08-09 10:46:27 -0700

    array of values for closest match calculated

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -15,13 +15,24 @@ module.exports = function (app) {
   app.post("/api/submit", function (req, res) {
     let newFriend = req.body;
     //CONVERT STRING NUM SCORES INTO NUM SCORES:
-    let strScores = req.body.scores;
-    let scores = strScores.map(score => Number(score));
-    newFriend.scores = scores;
+    let incomingStrScores = req.body.scores;
+    let incomingScores = incomingStrScores.map(score => Number(score));
+    newFriend.scores = incomingScores;
+
+    // IDENTIFY THE CLOSEST MATCH FOR NEWFRIEND AND SEND IT BACK:
+    let resultsArray = [];
+    surveyData.forEach(function(friend) {  
+      let magicNumber = 0;
+      for(let i = 0; i < friend.scores.length; i++) {
+        magicNumber += Math.abs(incomingScores[i] - friend.scores[i]);
+      }
+      resultsArray.push(magicNumber);
+    })
+    console.log(resultsArray);
+
     //PUSH NEWFRIEND ONTO SURVEYDATA:
     surveyData.push(newFriend);
-    console.log(surveyData);
-    // IDENTIFY THE CLOSEST MATCH AND SEND IT BACK (REPLACING NEW FRIEND BELOW):
+    //SEND BACK OBJECT FOR MATCHING FRIEND (survey.html will deal with the response it receives):
     res.json(newFriend);
   });
 
