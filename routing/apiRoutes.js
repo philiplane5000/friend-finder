364afb95f51134ad03b8be407b8d1a833adf4e28
philtd5000 <philiptaft9@gmail.com>
2018-08-09 09:30:15 -0700

    survey submission adds new friend

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,24 +1,33 @@
 let surveyData = require("../data/friends");
 
 
-module.exports = function(app) {
-  
-    //GET REQUESTS//
-    app.get("/api/friends", function(req, res) {
-        res.json(surveyData);
-    });
-    
-    
-    //POST REQUESTS//
-    app.post("/api/submit", function(req, res) {
-      console.log(surveyData);
-      surveyData.push(req.body);
-    });
-  
-    app.post("/api/clear", function() {
-      // Empty out the arrays of data
-      surveyData = [];  
-      console.log(surveyData);
-    });
+module.exports = function (app) {
 
-  };
+  //GET REQUESTS//
+  //*************/
+  app.get("/api/friends", function (req, res) {
+    res.json(surveyData);
+  });
+
+
+  //POST REQUESTS//
+  //**************/
+  app.post("/api/submit", function (req, res) {
+    let newFriend = req.body;
+    //CONVERT STRING NUM SCORES INTO NUM SCORES:
+    let strScores = req.body.scores;
+    let scores = strScores.map(score => Number(score));
+    //SET NEWFRIEND OBJECT SCORES TO NUM SCORES:
+    newFriend.scores = scores;
+    //PUSH NEWFRIEND ONTO SURVEYDATA:
+    surveyData.push(newFriend);
+    console.log(surveyData);
+  });
+
+  app.post("/api/clear", function () {
+    // Empty out the arrays of data
+    surveyData = [];
+    console.log(surveyData);
+  });
+
+};
